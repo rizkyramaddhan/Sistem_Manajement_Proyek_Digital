d76fd0b106a6972850fe7d06327edd58e18d5c86
rizkyramadhan <rizkyramaddhan648.com>
2026-04-09 16:32:30 +0700

    Initialisasi Projek

---
+++ src/model/Project.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Project {
+    private String name;
+    private ArrayList <Task> tasks;
+}
---
+++ src/model/Task.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Task {
+    private String title;
+    private String description;
+    private String status;
+
+    public Task(String title, String description){
+        this.title = title;
+        this.description = description;
+        this.status = "To Do";
+    }
+
+    // Getter and Setter
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void getDescription(String description){
+        this.description = description;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+}
